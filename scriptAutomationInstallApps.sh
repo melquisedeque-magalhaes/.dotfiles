36169cfef1132a3cc612634a6ca78734d26d8c1e
melquisedeque_sodre <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2022-09-04 23:56:21 -0300

    automatização de apps

---
+++ scriptAutomationInstallApps.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+directory=/home/melqui/.dotfiles/
+
+apt_install() {
+  apt_packages="$directory/packagesApt.txt"
+
+  while read line; do 
+    sudo apt install  $line -y
+  done < "$apt_packages"
+
+  exit
+}
+
+snap_install() {
+  while read line; do 
+    sudo snap install  $line
+  done < "$snap_packages"
+
+  apt_install
+}
+
+# download_zsh() {
+#   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#   clone_repos
+# }
+
+clone_repos() {
+  git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
+
+  exit
+}
+
+start() {
+  while :
+    do 
+     
+      snap_packages="$directory/packagesSnap.txt"
+      snap_install
+
+  done  
+
+  exit
+}
+
+start
